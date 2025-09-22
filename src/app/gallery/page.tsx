329176039014abea85392966860c052c10ad6a40
Riccardo Barone <riccardo.ComputerScience@gmail.com>
2025-09-22 12:30:57 +0100

    #31 added metadata for gallery page

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,6 +1,21 @@
 import React from "react";
 import Gallery from "components/galleryWall";
 import { Photos } from "assets/data";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "gallery",
+  description:
+    "Explore the vibrant moments of the Winchester Volleyball Club through our gallery. See thrilling matches, team celebrations, training sessions, and community events that capture the spirit and energy of our players. Stay connected and relive the excitement of every spike, serve, and victory.",
+
+  // Open Graph (social previews)
+  openGraph: {
+    title: "gallery",
+    description:
+      "Explore the vibrant moments of the Winchester Volleyball Club through our gallery. See thrilling matches, team celebrations, training sessions, and community events that capture the spirit and energy of our players. Stay connected and relive the excitement of every spike, serve, and victory.",
+    url: "https://www.winchestervolleyball.co.uk/gallery",
+  },
+};
 
 export default function GalleryPage() {
   return (
